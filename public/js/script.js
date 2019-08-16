fe9c79473842f66fe8e2a897b11833b2af1322bf
Lobachev27 <Lobachevd27@gmail.com>
2019-08-16 22:36:10 +0300

    Intro was added

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 100) {
+    if (scroll >= 350) {
       $(".header").addClass("scrolled");
     } else {
       $(".header").removeClass("scrolled");
@@ -26,4 +26,27 @@ $(document).ready(function() {
       $('body').addClass('ov-hid');
     }
   });
+});
+
+/*Параллакс*/
+
+(function($){
+  $.fn.bgscroll=function(options){
+    var x=$.extend({bgpositionx:50,direction:"bottom",debug:!1,min:0,max:100},options);
+    var a=$(document).height()-$(window).height(),b=a-(this.offset().top+this.height());
+    this.offset().top<a&&(b=0);
+    var c=(this.offset().top+this.height());
+    if($(window).scrollTop()>b && $(window).scrollTop()<c){
+      var d=($(window).scrollTop()-b)/(c-b)*100;
+      "top"==x.direction&&(d=100-d),d>x.max&&(d=x.max),d<x.min&&(d=x.min);
+      if(x.debug)console.log('Element background position: '+d+' %');
+    }
+    return this.css({
+      backgroundPosition: x.bgpositionx+'% '+d+'%'
+    });
+  };
+}(jQuery));
+
+$(window).scroll(function(){
+  $('.intro').bgscroll();
 });
